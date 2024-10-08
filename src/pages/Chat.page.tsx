12f884a2047ae2285d98dda37ca843d39eb1821e
BJM-Tue <t.lechinh@benjamin.co.jp>
2024-10-08 13:23:00 +0700

    fix: test rewrites & redirect amplify

--- src/pages/Chat.page.tsx
+++ src/pages/Chat.page.tsx
@@ -1,9 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const Chat = () => {
+  const [searchParams] = useSearchParams();
   const params = useParams();
 
-  return <div>Chat Page, params: {JSON.stringify(params)}</div>;
+  return (
+    <div>
+      Chat Page
+      <div>Querystring: {searchParams.toString()}</div>
+      <div>Params: {JSON.stringify(params)}</div>
+    </div>
+  );
 };
 
 export default Chat;
